ff804bb3d8745f9ec37e5172218703d57bcd2478
Peter Stanger <psta191@mywhitecliffe.com>
2020-06-01 12:43:53 +1200

    Revised solution after reviewing suggested answer

--- src/arraytasks/ArrayTasks.java
+++ src/arraytasks/ArrayTasks.java
@@ -5,6 +5,8 @@
  */
 package arraytasks;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Peter
@@ -14,6 +16,7 @@ public class ArrayTasks {
     /**
      * @param args the command line arguments
      */
+
     public static void main(String[] args) {
         
         //Task 1
@@ -26,9 +29,17 @@ public class ArrayTasks {
             System.out.println(each);
         };
         
-        String[] grades2 = new String[]{
+        String[] grades2 = new String[5];
+        
+        System.arraycopy(grades, 0, grades2, 0, 2);  //using arraycopy method
+        grades2[2] = "C";
+        System.arraycopy(grades, 2, grades2, 3, 2);  //using arraycopy method
+        
+        
+        /*String[] grades2 = new String[5]{
             grades[0],grades[1],"C",grades[2],grades[3]
-        };
+        };*/
+
         
         for ( String each : grades2 ){
             System.out.println(each);
@@ -42,9 +53,17 @@ public class ArrayTasks {
         
         System.out.println(subjects[1]);
         
+        /*
         String[] subjects2 = new String[]{
             subjects[0],subjects[1],subjects[2],"History","P.E."
         };
+        */
+        
+
+        String[] subjects2 = Arrays.copyOf(subjects, 5);  //Using copyOf method
+        
+        subjects2[3] = "History";
+        subjects2[4] = "Fine Arts";
         
         for ( String each : subjects2 ){
             System.out.println(each);
